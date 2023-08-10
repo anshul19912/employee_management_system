cc647b8c1959d92a48d8dfa4968d88cb37e62b1c
anshul19912 <anshulsingh19912@gmail.com>
2023-08-11 01:20:52 +0530

    final commit

--- src/main/java/com/example/employee_management_system/controller/EmployeeController.java
+++ src/main/java/com/example/employee_management_system/controller/EmployeeController.java
@@ -19,9 +19,8 @@ public class EmployeeController {
     //display list of Employees
     @RequestMapping("/")                            //or  @GetMapping("/")
     public String viewHomePage(Model model){
-//        model.addAttribute("listEmployees", employeeService.getAllEmployees());
-//        return "index";
-        return findPaginated(1, "firstName", "asc", model);
+        model.addAttribute("listEmployees", employeeService.getAllEmployees());           // key-value pair
+        return "index";
     }
 
     @GetMapping("/showNewEmployeeForm")
@@ -58,27 +57,7 @@ public class EmployeeController {
         return "redirect:/";
     }
 
-    @GetMapping("/page/{pageNo}")
-    public String findPaginated(@PathVariable(value = "pageNo") int pageNo,
-                                @RequestParam("sortField") String sortField,
-                                @RequestParam("sortDir") String sortDir,
-                                Model model) {
-        int pageSize = 5;
 
-        Page < Employee > page = employeeService.findPaginated(pageNo, pageSize, sortField, sortDir);
-        List < Employee > listEmployees = page.getContent();
-
-        model.addAttribute("currentPage", pageNo);
-        model.addAttribute("totalPages", page.getTotalPages());
-        model.addAttribute("totalItems", page.getTotalElements());
-
-        model.addAttribute("sortField", sortField);
-        model.addAttribute("sortDir", sortDir);
-        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
-
-        model.addAttribute("listEmployees", listEmployees);
-        return "index";
-    }
 
 
 }
